5051bdd758f62ad6187382b4b028eac79213ebab
Yongbin Feng <ybfeng94@gmail.com>
2024-04-08 00:40:20 -0500

    update onnx

--- README.md
+++ README.md
@@ -22,8 +22,10 @@ pip install tensorflow-rocm
 # install pytorch
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.7
 # install onnx runtime
-pip install https://download.onnxruntime.ai/onnxruntime_training-1.17.0.dev20231228002%2Brocm60-cp38-cp38-manylinux_2_28_x86_64.whl
+pip install https://download.onnxruntime.ai/onnxruntime_training-1.16.3%2Brocm55-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 ```
 
-Remaining issue: there seems to be some incompatiblity between TF and PyTorch on ROCm. `Import` would crash if both are imported in the same script.
+Remaining issue: 
+- there seems to be some incompatiblity between TF and PyTorch on ROCm. `Import` would crash if both are imported in the same script.
+- Recent versions of ONNX would crash on ROCm [issue here](https://github.com/microsoft/onnxruntime/issues/20203). The version above is the latest that works on ROCm 5.7 and ONNX 1.16.3.
 
--- directinference/test_onnx.py
+++ directinference/test_onnx.py
@@ -6,7 +6,7 @@ path = "/work1/yfeng/yfeng/sonic-models/models/particlenet_AK4/1/model.onnx"
 providers = [("ROCMExecutionProvider")]
 sess_options = rt.SessionOptions()
 sess_options.graph_optimization_level = rt.GraphOptimizationLevel.ORT_ENABLE_ALL
-sess = rt.InferenceSession(path, sess_options=sess_options, providers=providers)
+sess = rt.InferenceSession(path, sess_options=sess_options, providers=['ROCMExecutionProvider', 'CPUExecutionProvider'])
 print("provider: ", sess.get_providers())
 input_name0 = sess.get_inputs()[0].name
 input_name1 = sess.get_inputs()[1].name
@@ -17,18 +17,18 @@ input_name5 = sess.get_inputs()[5].name
 
 output_name = sess.get_outputs()[0].name
 
-nevts = 1
-#pf_points   = np.random.randn(nevts, 2,  100).astype(np.float32)
-#pf_features = np.random.randn(nevts, 20, 100).astype(np.float32)
-#pf_mask     = np.random.randn(nevts, 1,  100).astype(np.float32)
-#sv_points   = np.random.randn(nevts, 2,  10).astype(np.float32)
-#sv_features = np.random.randn(nevts, 11, 10).astype(np.float32)
-#sv_mask     = np.random.randn(nevts, 1,  10).astype(np.float32)
-pf_points = np.zeros((nevts, 2, 100)).astype(np.float32)
-pf_features = np.zeros((nevts, 20, 100)).astype(np.float32)
-pf_mask = np.zeros((nevts, 1, 100)).astype(np.float32)
-sv_points = np.zeros((nevts, 2, 10)).astype(np.float32)
-sv_features = np.zeros((nevts, 11, 10)).astype(np.float32)
-sv_mask = np.zeros((nevts, 1, 10)).astype(np.float32)
+nevts = 5000
+pf_points   = np.random.randn(nevts, 2,  100).astype(np.float32)
+pf_features = np.random.randn(nevts, 20, 100).astype(np.float32)
+pf_mask     = np.random.randn(nevts, 1,  100).astype(np.float32)
+sv_points   = np.random.randn(nevts, 2,  10).astype(np.float32)
+sv_features = np.random.randn(nevts, 11, 10).astype(np.float32)
+sv_mask     = np.random.randn(nevts, 1,  10).astype(np.float32)
+#pf_points = np.zeros((nevts, 2, 100)).astype(np.float32)
+#pf_features = np.zeros((nevts, 20, 100)).astype(np.float32)
+#pf_mask = np.zeros((nevts, 1, 100)).astype(np.float32)
+#sv_points = np.zeros((nevts, 2, 10)).astype(np.float32)
+#sv_features = np.zeros((nevts, 11, 10)).astype(np.float32)
+#sv_mask = np.zeros((nevts, 1, 10)).astype(np.float32)
 
 pred_onx = sess.run([output_name], {input_name0: pf_points, input_name1: pf_features, input_name2: pf_mask, input_name3: sv_points, input_name4: sv_features, input_name5: sv_mask})[0]
